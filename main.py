46813af497916e83bb86e540c15e5fd827e18e62
Rbuerk9522 <110101078+Rbuerk9522@users.noreply.github.com>
2025-12-09 11:57:59 +0000

    upodated main.py and local_api.py to bei nlinewith project rubric

--- main.py
+++ main.py
@@ -26,22 +26,20 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
-encoder = load_model(path)
+encoder_path = "model/encoder.pkl"
+encoder = load_model(encoder_path)
 
-path = None # TODO: enter the path for the saved model 
-model = load_model(path)
+model_path = "model/model.pkl"
+model = load_model(model_path)
 
 # TODO: create a RESTful API using FastAPI
-app = None # your code here
+app = app = FastAPI()
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
-    # your code here
-    pass
-
+    return {"message": "Hello"}
 
 # TODO: create a POST on a different path that does model inference
 @app.post("/data/")
@@ -65,10 +63,12 @@ async def post_inference(data: Data):
         "native-country",
     ]
     data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
+        data,
+        categorical_features=cat_features,
+        training=False,
+        encoder=encoder,
+        lb=None
     )
-    _inference = None # your code here to predict the result using data_processed
+
+    _inference = _inference = inference(model, data_processed)
     return {"result": apply_label(_inference)}
